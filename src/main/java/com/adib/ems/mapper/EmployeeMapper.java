7ac515e619310ecf2e1dfddfb35338ecb2e0f60a
Adib-Mahfuj <adibmahfuj420@gmail.com>
2025-05-06 18:19:03 +0600

    feat: add EmployeeDto and EmployeeMapper
    - Added EmployeeDto class for data transfer
    - Created EmployeeMapper to convert between Entity and DTO

---
+++ src/main/java/com/adib/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.adib.ems.mapper;
+
+import com.adib.ems.dto.EmployeeDto;
+import com.adib.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee)
+    {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto)
+    {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
