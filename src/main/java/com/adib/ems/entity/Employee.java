b169cb6ce571297cbc7c16c52b2b85f410eec827
Adib-Mahfuj <adibmahfuj420@gmail.com>
2025-05-06 13:51:36 +0600

    chore: setup database and create Employee entity
    - Configured datasource in application.properties
    - Created Employee class with @Entity annotation

---
+++ src/main/java/com/adib/ems/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.adib.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EMPLOYEES_SEQ")
+    @SequenceGenerator(name = "EMPLOYEES_SEQ", sequenceName = "EMPLOYEES_SEQ", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name ="email_id",nullable = false,unique = true)
+    private String email;
+
+}
